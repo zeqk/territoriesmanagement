aef10cbf70ae881f124f4704eefaf3bd0cbdfa1d
zeqk.net <zeqk.net@3ec0cc46-27d2-11de-9ca7-1beefeb68fa5>
2009-04-16 02:53:33 +0000

    frmMain

---
+++ trunk/territoriesmanagement/Territories.GUI/frmMain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Territories.GUI
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void mnuDepartments_Click(object sender, EventArgs e)
+        {
+            frmDepartments myForm = new frmDepartments();
+            myForm.Show();
+                
+        }
+
+        private void mnuCities_Click(object sender, EventArgs e)
+        {
+            frmCities myForm = new frmCities();
+            myForm.Show();
+        }
+    }
+}
