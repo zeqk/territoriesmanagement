12fabb88c662e3ed8e62897e8b42d10a2ca43384
zeqk.net <zeqk.net@3ec0cc46-27d2-11de-9ca7-1beefeb68fa5>
2009-05-04 20:00:53 +0000

    cambio de idea, comienzo del DataBridge y Reflection

---
+++ trunk/territoriesmanagement/Territories.DAL2/CommandExecutor.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.Objects;
+
+namespace Territories.DAL
+{
+    public class CommandExecutor
+    {
+        public T ExecuteFirstOrDefault<T>(ObjectQuery<T> objectQuery)
+        {
+            try
+            {
+                objectQuery.MergeOption = MergeOption.AppendOnly;
+                return objectQuery.FirstOrDefault();
+            }
+            catch (Exception ex)
+            {                
+                throw ex;
+            }
+        }
+
+        public T ExecuteFirstOrDefault<T>(IQueryable<T> L2Query)
+        {
+            try
+            {
+                return L2Query.FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<T> ExecuteList<T>(ObjectQuery<T> objectQuery)
+        {
+            try
+            {
+                objectQuery.MergeOption = MergeOption.AppendOnly;
+                return objectQuery.ToList();
+            }
+            catch (Exception ex)
+            {                
+                throw ex;
+            }
+
+        }
+
+        public List<T> ExecuteList<T>(IQueryable<T> L2Query)
+        {
+            try
+            {
+                return L2Query.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        public void SaveChanges(ObjectContext context)
+        {
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                
+                throw ex;
+            }
+        }
+    }
+}
