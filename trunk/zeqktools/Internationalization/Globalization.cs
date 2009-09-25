d8642ede10aafc0d7fddbf0d71a92bf6d65a92c4
zeqk.net <zeqk.net@3ec0cc46-27d2-11de-9ca7-1beefeb68fa5>
2009-09-25 14:49:32 +0000

    enums y controles creados orginalmente para territoriesmanagement

---
+++ trunk/zeqktools/Internationalization/Globalization.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
+using System.Globalization;
+using System.Resources;
+
+namespace ZeqkTools.Internationalization
+{
+	
+	
+    public class Globalization
+    {
+        private CultureInfo _culture;
+
+
+        public Globalization()
+        {
+            _culture = new CultureInfo("en-US");
+        }
+
+        public Globalization(string cultureStr)
+        {
+            _culture = new CultureInfo(cultureStr);
+        }
+
+        public CultureInfo Culture
+        {
+            get { return _culture; }
+            set { _culture = value; }
+        }
+	
+
+
+        public string GetString(Type type, string text)
+        {
+            string resourceStr = type.Namespace + ".Internacionalization." + type.Name;
+
+            ResourceManager rm = new ResourceManager(resourceStr, type.Assembly);
+
+            return rm.GetString(text, _culture);
+
+        }
+
+    }
+}
