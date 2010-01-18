64136c1070ded2ccdc283a242364aad1ff2b6b63
zeqk.net <zeqk.net@3ec0cc46-27d2-11de-9ca7-1beefeb68fa5>
2010-01-18 20:11:16 +0000

    Merge

--- trunk/zeqktools/ZeqkTools.ResourcesGenerator/frmResxGenerator.cs
+++ trunk/zeqktools/ZeqkTools.ResourcesGenerator/frmResxGenerator.cs
@@ -13,13 +13,14 @@ using System.Collections;
 namespace ZeqkTools.ResourcesGenerator
 {
     public partial class frmResxGenerator : Form
-    {
-        List<string> files = new List<string>();        
+    {   
 
         public frmResxGenerator()
         {
             InitializeComponent();
         }
+
+        #region GenerateResx
 
         private void btnGenerate_Click(object sender, EventArgs e)
         {
@@ -72,23 +73,25 @@ namespace ZeqkTools.ResourcesGenerator
 
                         foreach (var item in existingEntries)
                         {
-                            rxrw.AddResource(new ResXDataNode(item.Key.ToString(), item.Value.ToString()));
+                            rxrw.AddResource(item.Key.ToString(), item.Value);
                         }
 
                         string line = "";
                         int count = 0;
                         while ((line = sr.ReadLine()) != null)
                         {
-                            string msg = ExtractString(line, fromStr, toStr);
-                            if (msg != "")
+                            if (line.Contains(fromStr) && line.Contains(toStr))
                             {
-                                if (!existingEntries.ContainsKey(msg))
-                                {                                    
-                                    rxrw.AddResource(new ResXDataNode(msg, msg));
-                                    existingEntries.Add(msg, msg);
+                                string msg = ExtractString(line, fromStr, toStr);
+                                if (msg != "")
+                                {
+                                    if (!existingEntries.ContainsKey(msg))
+                                    {                                    
+                                        rxrw.AddResource(msg, msg);
+                                        existingEntries.Add(msg, msg);
+                                    }
+                                    count++;                                    
                                 }
-                                count++;
-                                
                             }
                         }
 
@@ -127,7 +130,7 @@ namespace ZeqkTools.ResourcesGenerator
 
             int firstIndex = text.IndexOf(from);
             firstIndex = firstIndex + from.Length;
-            int lastIndex = text.LastIndexOf(to);
+            int lastIndex = text.IndexOf(to);
             int lenght = lastIndex - firstIndex;
             if (lenght > 0)
                 rv = text.Substring(firstIndex, lenght);
@@ -153,7 +156,122 @@ namespace ZeqkTools.ResourcesGenerator
         private void btnSelectDestiny_Click(object sender, EventArgs e)
         {
             if(fbdDestiny.ShowDialog() == DialogResult.OK)
-                txtDestiny.Text = fbdDestiny.SelectedPath;   
+                txtDestiny.Text = fbdDestiny.SelectedPath;
         }
+        #endregion
+        #region Merge
+
+        private void btnMerge_Click(object sender, EventArgs e)
+        {
+            if (chkLstResxFiles.CheckedItems.Count == 0 ||
+                !txtMergedResxFile.Text.Contains(".resx"))
+            {
+                MessageBox.Show("Must be define all settings");
+            }
+            else
+            {
+                foreach (var item in chkLstResxFiles.CheckedItems)
+                {
+                    string file = item.ToString();
+                    string resxFile = txtMergedResxFile.Text;
+
+                    file = Path.Combine(txtSource.Text, file);
+                    
+
+                    try
+                    {
+                        Merge(file, resxFile);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+
+                }
+            }
+        }
+
+
+        private bool Merge(string file, string resxFile)
+        {
+            bool rv = false;
+
+            Dictionary<object, object> existingEntries = new Dictionary<object, object>();
+
+            try
+            {
+                if (File.Exists(resxFile))
+                    existingEntries = GetEntries(resxFile);
+
+
+                using (ResXResourceReader rxrr = new ResXResourceReader(file))
+                {
+                    using (ResXResourceWriter rxrw = new ResXResourceWriter(resxFile))
+                    {
+
+                        foreach (var item in existingEntries)
+                        {
+                            rxrw.AddResource(item.Key.ToString(), item.Value);
+                        }
+
+                        foreach (DictionaryEntry item in rxrr)
+                        {
+                            if (!existingEntries.ContainsKey(item.Key))
+                            {
+                                rxrw.AddResource(item.Key.ToString(), item.Value);
+                                existingEntries.Add(item.Key, item.Value);
+                            }
+                        }    
+
+                        rxrw.Close();
+                    }
+                }
+                rv = true;
+            }
+            catch (Exception ex)
+            {
+                if (ex.GetType() == typeof(System.IO.FileNotFoundException))
+                {
+                    throw new Exception("Incorrect file.", ex);
+                }
+                else
+                    throw ex;
+            }
+
+
+            return rv;
+        }
+
+        private void btnSelectMergedSource_Click(object sender, EventArgs e)
+        {
+            if (fbdDestiny.ShowDialog() == DialogResult.OK)
+            {
+                txtMergeSource.Text = fbdDestiny.SelectedPath;
+                txtMergedResxFile.Text = fbdDestiny.SelectedPath;
+                sfdMergedResx.InitialDirectory = fbdDestiny.SelectedPath;
+
+
+                DirectoryInfo dir = new DirectoryInfo(txtMergeSource.Text);
+                FileInfo[] fileList = dir.GetFiles("*.resx");
+                foreach (FileInfo item in fileList)
+                {
+                    chkLstResxFiles.Items.Add(item.Name);
+                }
+
+
+            }
+        }
+
+        private void btnSelectMergedResx_Click(object sender, EventArgs e)
+        {
+            if (sfdMergedResx.ShowDialog() == DialogResult.OK)
+            {
+                txtMergedResxFile.Text = Path.GetFullPath(sfdMergedResx.FileName);
+            }
+        }
+
+        #endregion
+
+        
     }
 }
