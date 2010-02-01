1f600de20f91eb62bceaf25ea219389752b451f3
zeqk.net <zeqk.net@3ec0cc46-27d2-11de-9ca7-1beefeb68fa5>
2010-02-01 01:33:07 +0000

    se configuró la versión y demas cosas

--- trunk/territoriesmanagement/TerritoriesManagement/Properties/AssemblyInfo.cs
+++ trunk/territoriesmanagement/TerritoriesManagement/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TerritoriesManagement")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("RinoxSoft")]
+[assembly: AssemblyCompany("Zeqk")]
 [assembly: AssemblyProduct("TerritoriesManagement")]
-[assembly: AssemblyCopyright("Copyright © RinoxSoft 2009")]
+[assembly: AssemblyCopyright("GNU GPL 2010  Zeqk")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.10.01.31")]
+[assembly: AssemblyFileVersion("1.10.01.31")]
