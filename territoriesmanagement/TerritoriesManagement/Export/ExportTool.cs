a7ef0ac05b1c5adb8a1d5fb43084a34b2b54290a
zeqk.net <zeqk.net@3ec0cc46-27d2-11de-9ca7-1beefeb68fa5>
2010-03-22 15:43:09 +0000

    sistema de intercambio de datos ahora se hace usando dataset y datatable

--- territoriesmanagement/TerritoriesManagement/Export/ExportTool.cs
+++ territoriesmanagement/TerritoriesManagement/Export/ExportTool.cs
@@ -11,10 +11,13 @@ using System.Drawing;
 using System.IO;
 using System.Xml;
 using System.Globalization;
-using GMap.NET;
 using System.Data.Objects;
 using System.Xml.Serialization;
 using System.Runtime.Serialization;
+using System.Data;
+using System.Web.UI.WebControls;
+using System.Web.UI;
+using GMap.NET;
 
 namespace TerritoriesManagement.Export
 {
@@ -33,43 +36,27 @@ namespace TerritoriesManagement.Export
         static public bool ExportToExcel(string path,string entity,string entitySet, string[] properties, string where, params ObjectParameter[] parameters)
         {
             bool rv = true;
-            Workbook book = new Workbook();
-            book.Worksheets.Add(entitySet);
-
-            
-
-            WorksheetStyle headerStyle = new WorksheetStyle("header");
-            headerStyle.Font.Bold = true;
-            headerStyle.Interior.Color = ColorTranslator.ToHtml(Color.Gray);
-            book.Styles.Add(headerStyle);
-
-
             try
             {
-                IList entities = Functions.GetEntities(entity, entitySet, where, parameters);
+                TerritoriesDataContext dm = new TerritoriesDataContext();
+                IList entities = Functions.GetEntities(dm, entity, entitySet, where, parameters);
 
-                WorksheetRow firstRow = book.Worksheets[entitySet].Table.Rows.Add();
+                DataTable table = RecordsToDataTable(entities, properties.ToList());
 
-                foreach (var propName in properties)
+                DataGrid grid = new DataGrid();
+                grid.HeaderStyle.Font.Bold = true;
+                grid.DataSource = table;
+                grid.DataBind();
+
+                // render the DataGrid control to a file
+
+                using (StreamWriter sw = new StreamWriter("c:\\test.xls"))
                 {
-                    firstRow.Cells.Add(propName, CarlosAg.ExcelXmlWriter.DataType.String, "header");
-                }
-
-                foreach (var item in entities)
-                {
-                    WorksheetRow row = book.Worksheets[entitySet].Table.Rows.Add();
-                    row.AutoFitHeight = true;
-                    
-
-                    foreach (var propName in properties)
+                    using (HtmlTextWriter hw = new HtmlTextWriter(sw))
                     {
-                        string strValue = Functions.GetPropertyValue(item, propName).ToString();
-
-                        WorksheetCell cell = row.Cells.Add(strValue);
+                        grid.RenderControl(hw);
                     }
                 }
-
-                book.Save(path);
             }
             catch (Exception ex)
             {
@@ -80,84 +67,115 @@ namespace TerritoriesManagement.Export
             return rv;
         }
 
+
+        static public void ToExcel(IList list, List<string> propertyList)
+        {
+            // create the DataGrid and perform the databinding
+
+            System.Web.UI.WebControls.DataGrid grid =
+                        new System.Web.UI.WebControls.DataGrid();
+            grid.HeaderStyle.Font.Bold = true;
+            grid.DataSource = list;
+
+            grid.DataBind();
+
+            // render the DataGrid control to a file
+
+            using (StreamWriter sw = new StreamWriter("c:\\test.xls"))
+            {
+                using (System.Web.UI.HtmlTextWriter hw = new System.Web.UI.HtmlTextWriter(sw))
+                {
+                    
+                    grid.RenderControl(hw);
+                }
+            }
+        }
         public void ExportToGMap(string path, string where, params ObjectParameter[] parameters)
         {
-            StreamWriter sw = new StreamWriter(path, false, Encoding.UTF8, 512);
-            using (XmlTextWriter xw = new XmlTextWriter(sw))
+            try
             {
-                xw.Formatting = Formatting.Indented;
-                xw.QuoteChar = char.Parse("'");
-                xw.WriteStartElement("markers");//open MARKERS
-                IList addresses = Functions.GetEntities("Address", "Addresses", where, parameters);
-
-
-                List<Address> addressList = (List<Address>)addresses;
-
-                int llenos = 0;
-                int vacios = 0;
-
-                CultureInfo culture = new CultureInfo("en-US");
-
-                foreach (var a in addressList)
+                TerritoriesDataContext dm = new TerritoriesDataContext();
+            
+                StreamWriter sw = new StreamWriter(path, false, Encoding.UTF8, 512);
+                using (XmlTextWriter xw = new XmlTextWriter(sw))
                 {
-                    if (a.Lat.HasValue && a.Lng.HasValue)
+                    xw.Formatting = Formatting.Indented;
+                    xw.QuoteChar = char.Parse("'");
+                    xw.WriteStartElement("markers");//open MARKERS
+                    IList addresses = Functions.GetEntities(dm, "Address", "Addresses", where, parameters);
+
+
+                    List<Address> addressList = (List<Address>)addresses;
+
+                    int llenos = 0;
+                    int vacios = 0;
+
+                    CultureInfo culture = new CultureInfo("en-US");
+
+                    foreach (var a in addressList)
                     {
-                        xw.WriteStartElement("marker");//open MARKER
+                        if (a.Lat.HasValue && a.Lng.HasValue)
+                        {
+                            xw.WriteStartElement("marker");//open MARKER
 
-                        xw.WriteAttributeString("address", a.Street + a.Number);
+                            xw.WriteAttributeString("address", a.Street + a.Number);
 
-                        xw.WriteAttributeString("lat", a.Lat.Value.ToString(culture));
-                        xw.WriteAttributeString("lng", a.Lng.Value.ToString(culture));
+                            xw.WriteAttributeString("lat", a.Lat.Value.ToString(culture));
+                            xw.WriteAttributeString("lng", a.Lng.Value.ToString(culture));
 
-                        xw.WriteAttributeString("id", a.IdAddress.ToString());
+                            xw.WriteAttributeString("id", a.IdAddress.ToString());
 
-                        xw.WriteEndElement();//close MARKER
-                        llenos++;
+                            xw.WriteEndElement();//close MARKER
+                            llenos++;
+                        }
+                        else
+                            vacios++;
+
                     }
-                    else
-                        vacios++;
+                    List<ZeqkTools.GeoPoint> points = addressList.Where(a => a.Lat.HasValue && a.Lng.HasValue)
+                                                          .Select(a => new ZeqkTools.GeoPoint(a.Lat.Value, a.Lng.Value)).ToList();
 
+                   
+                    var aux = ZeqkTools.Functions.CalculateMiddlePoint(points);
+                    PointLatLng point = new PointLatLng(aux.Lat, aux.Lng);
+                    xw.WriteStartElement("centerPoint");
+                    xw.WriteAttributeString("lat", point.Lat.ToString(new CultureInfo("en-US")));
+                    xw.WriteAttributeString("lng", point.Lng.ToString(new CultureInfo("en-US")));
+                    xw.WriteEndElement();
+
+                    xw.WriteEndElement(); //close MARKERS
+
+                    sw.Close();
                 }
-                List<ZeqkTools.GeoPoint> points = addressList.Where(a => a.Lat.HasValue && a.Lng.HasValue)
-                                                      .Select(a => new ZeqkTools.GeoPoint(a.Lat.Value, a.Lng.Value)).ToList();
-
-               
-                var aux = ZeqkTools.Functions.CalculateMiddlePoint(points);
-                PointLatLng point = new PointLatLng(aux.Lat, aux.Lng);
-                xw.WriteStartElement("centerPoint");
-                xw.WriteAttributeString("lat", point.Lat.ToString(new CultureInfo("en-US")));
-                xw.WriteAttributeString("lng", point.Lng.ToString(new CultureInfo("en-US")));
-                xw.WriteEndElement();
-
-                xw.WriteEndElement(); //close MARKERS
-
-                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
             }
 
         }
 
+
+        #region ExportData
         static public void ExportData(string path, List<string> entityList)
         {
             try
             {
-                XmlWriterSettings setting = new XmlWriterSettings();
-                setting.Indent = true;                    
-                XmlWriter xtw = XmlTextWriter.Create(path, setting);
-                XmlDictionaryWriter xdw = XmlDictionaryWriter.CreateDictionaryWriter(xtw);
-                DataContractSerializer dcs = new DataContractSerializer(typeof(List<Hashtable>));
-
+                TerritoriesDataContext dm = new TerritoriesDataContext();
+                DataSet ds = new DataSet("TerritoriesManagementExchangeFile");
+                
                 foreach (var entityName in entityList)
                 {
                     string entitySetName = Functions.GetEntitySetNameByEntityName(entityName);
 
-                    IList records = Functions.GetEntities(entityName,entitySetName,"");
-                    
-                    IList tmxLst = RecordsToTmxFormat(records);
-                    dcs.WriteObject(xdw, tmxLst);
-                    xtw.Close();
+                    IList records = Functions.GetEntities(dm, entityName,entitySetName,"");
+                    List<Property> propLst = Functions.GetPropertyListByType(records[0].GetType());
+                    DataTable dt = RecordsToDataTable(records, propLst);
+                    dt.TableName = entitySetName;
+                    ds.Tables.Add(dt);
                 }
 
-                
+                ds.WriteXml(path, XmlWriteMode.WriteSchema);
             }
             catch (Exception ex)
             {
@@ -165,35 +183,62 @@ namespace TerritoriesManagement.Export
             }
         }
 
-        static private IList RecordsToTmxFormat(IList records)
+        static private DataTable RecordsToDataTable(IList records, List<Property> propLst)
         {
-            IList rv = new List<Hashtable>();
-
-            foreach (object item in records)
+            DataTable dt = new DataTable();
+            
+            if (records.Count > 0)
             {
-                rv.Add(ObjToTmxFormat(item));
+                foreach (Property item in propLst)
+                {
+                    if (!Functions.IsNullableType(item.Type))
+                        dt.Columns.Add(item.Name,item.Type);
+                    else
+                        dt.Columns.Add(item.Name, Functions.NullableToType(item.Type));
+                }
+
+                foreach (object item in records)
+                {
+                    dt.NewRow();
+                    dt.Rows.Add(ObjToDataRow(item, dt.NewRow()));
+                }
             }
 
-            return rv;
+            return dt;
         }
 
-
-        static private Hashtable ObjToTmxFormat(object obj)
+        static private DataTable RecordsToDataTable(IList records, List<string> propLst)
         {
-            Hashtable rv = new Hashtable();
+            DataTable dt = new DataTable();
 
-            List<string> propLst = Functions.GetPropertyListByType(obj.GetType());
-
-            foreach (string propName in propLst)
+            if (records.Count > 0)
             {
-                object value = Functions.GetPropertyValue(obj, propName);
-                rv.Add(propName, value);
+                foreach (string item in propLst)
+                {
+                    dt.Columns.Add(item);
+                }
+
+                foreach (object item in records)
+                {
+                    dt.NewRow();
+                    dt.Rows.Add(ObjToDataRow(item, dt.NewRow()));
+                }
             }
 
-
-            return rv;
+            return dt;
         }
-        
+
+        static private DataRow ObjToDataRow(object obj, DataRow row)
+        {
+            foreach (DataColumn column in row.Table.Columns)
+            {
+                object value = Functions.GetPropertyValue(obj, column.ColumnName);
+                row.SetField(column, value);
+            }
+
+            return row;
+        }
+        #endregion
 
     }
 }
