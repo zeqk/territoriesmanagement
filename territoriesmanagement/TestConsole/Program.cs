e6aa960a1cc5d78d24f0602975275baa9a878145
zeqk.net <zeqk.net@3ec0cc46-27d2-11de-9ca7-1beefeb68fa5>
2010-06-21 05:52:45 +0000

    - escritura de georss - mapa portalbe html

--- territoriesmanagement/TestConsole/Program.cs
+++ territoriesmanagement/TestConsole/Program.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections;
-using System.Linq;
 using System.Text;
 using System.Data.Entity;
 using TerritoriesManagement.Model;
 using TerritoriesManagement;
 using GMap.NET;
 using System.Globalization;
+using System.Xml.Linq;
 using System.Xml;
 
 namespace TestConsole
@@ -16,9 +16,25 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
+            prueba3();
+        }
 
-            getStatics();
+        static void WriteNames()
+        {
+            Console.ReadKey();
+            TerritoriesDataContext dm = new TerritoriesDataContext();
 
+            var territories = dm.Territories;
+
+            foreach (Territory t in territories)
+            {
+                t.Name = t.Name.Substring(8);
+                Console.WriteLine(t.Name);
+                dm.ApplyPropertyChanges("Territories", t);
+                
+            }
+            dm.SaveChanges();
+            Console.ReadKey();
 
         }
 
@@ -162,6 +178,52 @@ namespace TestConsole
             doc.AppendChild(rssNode);
 
             doc.Save(@"D:\prueba.xml");
+        }
+
+        static void prueba3()
+        {
+
+            TerritoriesDataContext dm = new TerritoriesDataContext();
+            var territories = dm.Territories;
+
+            XNamespace nsGeorss = "http://www.georss.org/georss";
+            XNamespace nsGml = "http://www.opengis.net/gml";
+
+            XElement channel = new XElement("channel",
+                new XElement("title", "Club Events Feed"),
+                new XElement("link", "www.google.com.ar"),
+                new XElement("description", "Events coming up..."));
+
+            XElement xmlFeed = new XElement("rss",
+                new XAttribute("version", 2.0),
+                new XAttribute(XNamespace.Xmlns + "georss", nsGeorss),
+                new XAttribute(XNamespace.Xmlns + "gml", nsGml),
+                channel);
+
+            foreach (var t in territories)
+            {
+                XElement polygon = new XElement(nsGml + "Polygon",
+                    new XElement(nsGml + "exterior",
+                        new XElement(nsGml + "LinearRing",
+                            new XElement(nsGml + "posList", t.Area)
+                            )
+                        )
+                    );
+
+                XElement element = new XElement("item",
+                    new XElement("title", t.Name),
+                    new XElement("link", new XAttribute("rel", "via"), new XAttribute("href", "www.google.com")),
+                    new XElement("description", t.Name),
+                    new XElement("content",t.Name),
+                    new XElement("author", "zeqk"),
+                    polygon
+                    );
+
+                channel.Add(element);
+            }
+
+            xmlFeed.Save(@"D:\prueba.xml");
+
         }
 
         #endregion
