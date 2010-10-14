c29be633801dd0a60016c7b0fcdad9a3d2ab986c
zeqk.net <zeqk.net@3ec0cc46-27d2-11de-9ca7-1beefeb68fa5>
2010-10-14 20:26:28 +0000

    nueva carpeta

---
+++ territoriesmanagement/TerritoriesManagement.GUI/Configuration/Config.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Windows.Forms;
+
+namespace TerritoriesManagement.GUI.Configuration
+{
+    [Serializable]
+    public class Config
+    {
+        static private string configFile = AppDomain.CurrentDomain.BaseDirectory + "config.xml";
+
+        static public string Language;
+
+        static public string Region;
+
+        static public string DefaultPlace;
+
+        static public GMap.NET.MapType MapType;
+
+        public Config()
+        {
+            Language = "English";
+            MapType = GMap.NET.MapType.GoogleMap;
+        }
+
+        static public void SaveConfig()
+        {
+            SaveConfig(configFile);
+        }
+
+        static public void SaveConfig(string path)
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlNode configNode = doc.CreateElement("Configuration");
+
+            XmlNode languageNode = doc.CreateElement("Language");
+            languageNode.InnerText = Language;
+            XmlNode mapTypeNode = doc.CreateElement("MapType");
+            mapTypeNode.InnerText = MapType.ToString();
+            XmlNode regionNode = doc.CreateElement("Region");
+            regionNode.InnerText = Region;
+            XmlNode defaultPlace = doc.CreateElement("DefaultPlace");
+            defaultPlace.InnerText = DefaultPlace;
+
+            configNode.RemoveAll();
+            configNode.AppendChild(languageNode);
+            configNode.AppendChild(mapTypeNode);
+            configNode.AppendChild(regionNode);
+            configNode.AppendChild(defaultPlace);
+
+            doc.AppendChild(configNode);
+
+            doc.Save(path);
+
+        }
+
+        static public void LoadSavedConfig()
+        {
+            LoadSavedConfig(configFile);
+        }
+
+        static public void LoadSavedConfig(string path)
+        {
+            try
+            {
+                if (File.Exists(path))
+                {
+                    XmlDocument doc = new XmlDocument();
+                    doc.Load(path);
+
+                    XmlNode configNode = doc.SelectSingleNode("Configuration");
+
+                    XmlNode languageNode = configNode.SelectSingleNode("Language");
+                    XmlNode mapTypeNode = configNode.SelectSingleNode("MapType");
+                    XmlNode regionNode = configNode.SelectSingleNode("Region");
+                    XmlNode defaultPlaceNode = configNode.SelectSingleNode("DefaultPlace");
+
+                    Language = languageNode.InnerText;
+                    MapType = (GMap.NET.MapType)Enum.Parse(typeof(GMap.NET.MapType), mapTypeNode.InnerText, true);
+                    Region = regionNode.InnerText;
+                    DefaultPlace = defaultPlaceNode.InnerText;
+                }
+                else
+                    LoadDefaultConfig();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                LoadDefaultConfig();
+            }            
+        }
+
+        static private void LoadDefaultConfig()
+        {
+            Language = "English";
+            MapType = GMap.NET.MapType.GoogleMap;            
+        }
+	
+    }
+}
