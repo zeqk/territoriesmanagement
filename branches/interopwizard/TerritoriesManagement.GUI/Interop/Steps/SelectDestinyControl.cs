d9e104c480484c151f07af0e4483e344a4828a02
zeqk.net <zeqk.net@3ec0cc46-27d2-11de-9ca7-1beefeb68fa5>
2010-12-13 20:20:25 +0000

    - código del wizard ordenado - exportación comienza a ser asincrónica

---
+++ branches/interopwizard/TerritoriesManagement.GUI/Interop/Steps/SelectDestinyControl.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TerritoriesManagement.GUI.Interop.Steps
+{
+    public partial class SelectDestinyControl : UserControl
+    {
+
+        public string FileName
+        {
+            get { return Path.GetFullPath(txtDestiny.Text); }
+        }
+        public string Filter
+        {
+            get { return saveFileDialog.Filter; }
+            set { saveFileDialog.Filter = value; }
+        }
+	
+	
+
+        public SelectDestinyControl()
+        {
+            InitializeComponent();
+        }
+
+        private void brnBrowse_Click(object sender, EventArgs e)
+        {            
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtDestiny.Text = Path.GetFullPath(saveFileDialog.FileName);
+            }
+        }
+    }
+}
